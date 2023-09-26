c484ba410bdb59aaed41458fd38f44dfdbe42dbb
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2023-09-26 06:36:25 +0300

    ahmed

---
+++ calculator.py
@@ -0,0 +1,20 @@
+x =int ( input("what's x? "))
+y =int ( input("what's y? "))
+
+z = x + y
+print(z);
+
+ 
+#the next operation with float
+x =float ( input("what is x?" ))
+y =float ( input("what is y?" ))
+
+z = x + y
+print(z);
+
+# calculate using round
+x =float ( input("what's x? "))
+y =float ( input("what's y? "))
+
+z =round(x / y, 2)
+print(z);
